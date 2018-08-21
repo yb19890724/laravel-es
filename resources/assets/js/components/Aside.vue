9c9a9b2ddbf5f3b41ffcf85397e482aa659fc1cd
hyb <354037968@qq.com>
2018-08-21 23:16:55 +0800

    跟新依赖

---
+++ resources/assets/js/components/Aside.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ul class="list-group">
+            <li class="list-group-item">商品列表(名称搜索)</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+
+
+    }
+</script>
--- resources/assets/js/components/ListView.vue
+++ resources/assets/js/components/ListView.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-2 ">
                 <ul class="list-group">
-                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">商品列表(名称搜索)</li>
                     <li class="list-group-item">Dapibus ac facilisis in</li>
                     <li class="list-group-item">Morbi leo risus</li>
                     <li class="list-group-item">Porta ac consectetur ac</li>
